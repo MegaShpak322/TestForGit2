410860d7ab9b2745a67af4062e5cd57b008f92e8
MegaShpak322 <molka.dmitry93@gmail.com>
2025-01-23 14:51:17 +0200

    Выполнил правки

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -2,6 +2,12 @@ package lesson1;
 
 public class Homework1 {
     public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hi");
+        test1();
+    }
 
+    private static void test1() {
+        System.out.println("Решение первой задачи");
     }
 }
